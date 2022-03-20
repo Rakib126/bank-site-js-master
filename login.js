0683d0e5860283bc0021c16e1aa9a185029e1082
Md.RakibulHasan <79445107+Rakib126@users.noreply.github.com>
2022-03-20 23:09:03 +0600

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.getElementById('login-submit').addEventListener('click', function () {
   const passwordField = document.getElementById('user-password');
   const userPassword = passwordField.value;
 
-  if (userEmail == 'ryasin420@gmail.com' && userPassword == 'secret') {
+  if (userEmail == 'Hasan12606@gmail.com' && userPassword == 'secret') {
     window.location.href = 'bank.html';
   } else {
     alert('Please enter right email and password.');
